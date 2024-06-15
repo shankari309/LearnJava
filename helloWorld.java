506acbe826f01a9789942c28d59b1dbe2614c8b0
shankari <shankaris3009@gmail.com>
2024-06-15 11:32:28 +0530

    Added info

--- helloWorld.java
+++ helloWorld.java
@@ -2,4 +2,8 @@ public class helloWorld{
     public static void main(String[] args){
         System.out.println("Hello World");
     }
+
+     // public- can be accessed by any where 
+    // static- no need to instantiate object to invoke the method declared with static
+    // main - where the JVM starts its execution
 }
