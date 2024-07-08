e5297d55d0500984745705fd0bc73d37b2697b5f
shankari <shankaris3009@gmail.com>
2024-07-08 22:28:45 +0530

    Started oops

---
+++ ClassSample.java
@@ -0,0 +1,8 @@
+public class ClassSample {
+    int id; //instance variable
+    public static void main(String args[]){
+        ClassSample c=new ClassSample();
+        System.out.println(c.id);;
+    }
+}
+
